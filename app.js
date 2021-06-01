d4abf5191a815275d09b78f06d140744ce432d16
Mayank Mittal <mayankmittal1410@gmail.com>
2021-06-01 22:14:40 +0530

    Testing

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/users');
 const MongoStore = require("connect-mongo");
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;// || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
